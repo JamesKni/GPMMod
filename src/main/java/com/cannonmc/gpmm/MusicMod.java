aef00ef27970a4830cfdd02a5e1b371b86c8e286
James Knight <jimdav40@gmail.com>
2018-07-24 08:28:32 +0100

    Moved json lib

--- src/main/java/com/cannonmc/gpmm/MusicMod.java
+++ src/main/java/com/cannonmc/gpmm/MusicMod.java
@@ -32,7 +32,7 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 public class MusicMod
 {
     public static final String MODID = "gpmm";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.9";
     private static final Minecraft mc = Minecraft.getMinecraft();
     public static boolean outdated = false;
     
