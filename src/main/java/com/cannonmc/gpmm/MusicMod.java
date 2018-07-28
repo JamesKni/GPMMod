4d0dfc006c01766f8592984a671092aa6450f440
james <james@james-ubuntu>
2018-07-28 14:39:57 +0100

    Release 1.1.1

--- src/main/java/com/cannonmc/gpmm/MusicMod.java
+++ src/main/java/com/cannonmc/gpmm/MusicMod.java
@@ -33,7 +33,7 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 public class MusicMod
 {
     public static final String MODID = "gpmm";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.1";
     public static final String ACCEPTED_VERSIONS = "[1.8, 1.8.9]";
     private static final Minecraft mc = Minecraft.getMinecraft();
     public static boolean outdated = false;
