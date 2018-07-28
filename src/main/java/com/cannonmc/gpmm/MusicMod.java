8293feeef86860b50482c28976dc068852019791
james <james@james-ubuntu>
2018-07-28 14:50:36 +0100

    Changed to 1.2

--- src/main/java/com/cannonmc/gpmm/MusicMod.java
+++ src/main/java/com/cannonmc/gpmm/MusicMod.java
@@ -33,7 +33,7 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 public class MusicMod
 {
     public static final String MODID = "gpmm";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.2";
     public static final String ACCEPTED_VERSIONS = "[1.8, 1.8.9]";
     private static final Minecraft mc = Minecraft.getMinecraft();
     public static boolean outdated = false;
@@ -66,7 +66,6 @@ public class MusicMod
     	configFile = event.getSuggestedConfigurationFile();
     	Config.init(configFile);
     	UpdateCheck.versionCheck();
-    	
     	OSFinder();
     }
     
